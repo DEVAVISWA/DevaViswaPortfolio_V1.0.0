84492ecb0ebd7994209fab7d2eb723e483a56be8
DEVAVISWA <devaviswa15@gmail.com>
2024-03-16 01:58:34 +0530

    skills done

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,9 +1,11 @@
 import Herosection from "../Herosection";
+import MySkills from "../MySkills";
 
 export default function Home() {
   return (
     <>
       <Herosection />
+      <MySkills />
     </>
   );
 }
